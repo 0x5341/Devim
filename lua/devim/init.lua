f63d27c33608781882fa546ba2eeb236a2e81e44
0x5341 <107183580+0x5341@users.noreply.github.com>
2025-08-01 11:44:43 +0900

    setup treesitter

--- lua/devim/init.lua
+++ lua/devim/init.lua
@@ -10,12 +10,21 @@ M.config_default = {
     shell = "bash"
   },
   treesitter = {
-    install = {}
+    install = { "lua", "markdown", "markdown_inline" }
   },
 }
 
+function M.merger(_, prev, value)
+  if vim.islist(prev) and vim.islist(value) then
+    local cp = vim.deepcopy(prev)
+    return vim.list_extend(cp, value)
+  else
+    return value
+  end
+end
+
 function M.setup(config)
-  local merged = vim.tbl_deep_extend("force", M.config_default, config)
+  local merged = vim.tbl_deep_extend(M.merger, M.config_default, config)
   require("devim.config").setup(merged)
 
   vim.g.__devim_config = merged
--- lua/devim/plugin/treesitter.lua
+++ lua/devim/plugin/treesitter.lua
@@ -8,6 +8,24 @@ return {
     config = function(_, opts)
       require("nvim-treesitter").setup(opts)
       require("nvim-treesitter").install(vim.g.__devim_config.treesitter.install)
+
+      -- autocmds
+      local aug = vim.api.nvim_create_augroup("devim-treesitter", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = aug,
+        pattern = "*",
+        callback = function(ev)
+          local lang_name = vim.treesitter.language.get_lang(ev.match) or ev.match
+          if vim.treesitter.language.add(lang_name) then
+            vim.treesitter.start()
+            vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end
+      })
     end
-  }
+  },
+  {
+
+  },
 }
