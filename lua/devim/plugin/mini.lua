7be58f77aa0d3ce0d18099beda23f14a13e98b6f
0x5341 <107183580+0x5341@users.noreply.github.com>
2025-08-05 12:25:16 +0900

    switch mini.pairs to nvim-autopairs

--- lua/devim/plugin/mini.lua
+++ lua/devim/plugin/mini.lua
@@ -15,12 +15,6 @@ return {
     opts = {}
   },
   {
-    "echasnovski/mini.pairs",
-    event = { "InsertEnter" },
-    version = false,
-    opts = {}
-  },
-  {
     "echasnovski/mini.surround",
     keys = {
       { "sa", mode = "n" },
